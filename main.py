7fd1bc1d076951d39b37b28b0dcf697670a316bd
Marina <marina311@mail.ru>
2020-04-30 19:31:04 +0500

    бот time

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 from auth_data import *
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, CommandHandler, Filters
+import time
 
 
-# Определяем функцию-обработчик сообщений.
-def echo(update, context):
-    update.message.reply_text('Я получил сообщение "{}".'.format(update.message.text))
+def echo(bot, update):
+    update.message.reply_text(update.message.text)
+
+
+def start(update, context):
+    update.message.reply_text("Привет! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!")
+
+
+def help(update, context):
+    update.message.reply_text("Я пока не умею помогать... Я только ваше эхо.")
+
+
+def get_time(update, context):
+    update.message.reply_text(time.asctime().split(" ")[3])
+
+
+def get_date(update, context):
+    update.message.reply_text(", ".join(time.asctime().split(" ")[1:3]))
 
 
 def main():
-    updater = Updater(TOKEN, use_context=True)
+    updater = Updater("YOUR_TOKEN", use_context=True)
 
     dp = updater.dispatcher
-    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    text_handler = MessageHandler(Filters.text, echo)
+    dp.add_handler(text_handler)
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("time", get_time))
+    dp.add_handler(CommandHandler("date", get_date))
 
     updater.start_polling()
     print('Bot started')
